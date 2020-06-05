8dc8a795a62be40c174a7629ec7e954bef29f170
Iann Santos <iannsantos8@gmail.com>
2020-06-05 17:17:18 -0300

    feat: initial pact config

--- client-api/src/server.ts
+++ client-api/src/server.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
-import routes from './routes';
+import routes from './routes/index';
+import startMockService from './services/Pact';
 
 const APP_PORT = 3333;
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.listen(APP_PORT, () => console.log(`[SERVER] Running on port ${APP_PORT}`));
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(routes);
+startMockService();
--- client-api/src/services/GetBalanceByClientID.spec.ts
+++
@@ -1,3 +0,0 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
-});
