dec57b6513d0caa85e9070987ef30fbafdb9ea81
Iann Santos <iannsantos8@gmail.com>
2020-06-08 13:38:59 -0300

    feat: add generate pact on client api

--- client-api/src/server.ts
+++ client-api/src/server.ts
@@ -1,7 +1,6 @@
 import cors from 'cors';
 import express from 'express';
 import routes from './routes/index';
-import startMockService from './services/Pact';
 
 const APP_PORT = 3333;
 
@@ -12,4 +11,3 @@ app.listen(APP_PORT, () => console.log(`[SERVER] Running on port ${APP_PORT}`));
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-startMockService();
